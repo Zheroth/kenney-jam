f3728c42c4e80d847fcf209b7c29a0527905f3bb
Jonathan Valle <riaj06@gmail.com>
2019-08-11 01:32:35 +0900

    Added GameManager

---
+++ Assets/Scripts/General/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Framework.Utils;
+using Rewired;
+
+public class GameManager : MonoSingleton<GameManager>
+{
+    public Player[] playerArr = new Player[4];
+
+    void Update()
+    {
+        IList<Joystick> joystickLists = ReInput.controllers.Joysticks;
+        for (int i = 0; i < joystickLists.Count; i++)
+        {
+            if (joystickLists[i].GetAnyButtonDown())
+            {
+                SpawnPlayer(joystickLists[i].id);
+            }
+        }
+    }
+
+    public void SpawnPlayer(int playerId)
+    {
+        Debug.Log("Spawn player: "+playerId);
+    }
+}
