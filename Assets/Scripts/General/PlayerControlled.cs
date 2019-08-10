2794865824578f520aec8c5e3b501baee6080f9c
Alistair O'Brien <alistair@pixelreverie.net>
2019-08-10 21:36:27 +0900

    Added Debug Player ID to Controller

--- Assets/Scripts/General/PlayerControlled.cs
+++ Assets/Scripts/General/PlayerControlled.cs
@@ -8,6 +8,8 @@ using UnityEngine.Experimental.PlayerLoop;
 [RequireComponent(typeof(CastleShip))]
 public class PlayerControlled : MonoBehaviour
 {
+    [SerializeField]
+    private int DEBUG_player = 0;
     private Player playerRef;
 
     private float deadZone = 0.1f;
@@ -29,7 +31,7 @@ public class PlayerControlled : MonoBehaviour
     void Awake()
     {
         //DEBUG
-        AssignPlayer(0);
+        AssignPlayer(DEBUG_player);
     }
 
     void Update()
