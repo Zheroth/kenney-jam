21cebfa6bd29a52620c9370dcf010e22c76a368f
Alistair O'Brien <alistair@pixelreverie.net>
2019-08-10 22:49:47 +0900

    Working on UI

---
+++ Assets/Scripts/General/UICombinedFilledImage.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UICombinedFilledImage : MonoBehaviour
+{
+    [SerializeField]
+    private Image[] images;
+    [SerializeField]
+    [Range(0,1)]
+    private float fillPercentage = 1;
+
+    private void OnValidate()
+    {
+        SetFillAmount(fillPercentage);
+    }
+
+    private void SetFillAmount(float value)
+    {
+        fillPercentage = value;
+
+        float division = 1.0f / images.Length;
+
+        for (int i = 0; i < images.Length; i++)
+        {
+            Image image = images[i];
+            float section = (i * division);
+            float fillLevel = MapValue(section, section + division, 0, 1, fillPercentage);
+            image.fillAmount = fillLevel;
+        }
+    }
+
+    public float MapValue(float a0, float a1, float b0, float b1, float a)
+    {
+        return b0 + (b1 - b0) * ((a - a0) / (a1 - a0));
+    }
+
+}
