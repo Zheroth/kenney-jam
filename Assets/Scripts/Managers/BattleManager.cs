113a7adf18760993e1c4f5d2c02ea1d73ae360dd
Jonathan Valle <riaj06@gmail.com>
2019-08-11 15:07:04 +0900

    Updated player1 join controller to only keyboard. Added spawning of ships.

---
+++ Assets/Scripts/Managers/BattleManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleManager : MonoBehaviour
+{
+    [SerializeField] private List<CastleShip> availableCastleShips;
+
+    private Dictionary<CastleShip.CastleShipType, GameObject> shipDict = new Dictionary<CastleShip.CastleShipType, GameObject>();
+
+    public bool doStuff;
+
+    void Start()
+    {
+        for(int i=0; i<availableCastleShips.Count; i++)
+        {
+            if (!shipDict.ContainsKey(availableCastleShips[i].shipType))
+            {
+                shipDict.Add(availableCastleShips[i].shipType, availableCastleShips[i].gameObject);
+            }
+        }
+    }
+
+    void Update()
+    {
+        if (doStuff)
+        {
+            SpawnShip(CastleShip.CastleShipType.Assaulter, 0, new Vector3(170,50,45));
+            doStuff = false;
+        }
+    }
+
+    public void SpawnShip(CastleShip.CastleShipType shipType, int playerId, Vector3 position)
+    {
+        GameObject newShip = Instantiate(shipDict[shipType], position, Quaternion.identity);
+        newShip.GetComponent<PlayerControlled>().AssignPlayer(playerId);
+    }
+
+
+}
