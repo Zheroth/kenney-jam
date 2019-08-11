82361f051c51bbdb0552cf4d529ddf3e04888cdd
Alistair O'Brien <alistair@pixelreverie.net>
2019-08-11 14:53:32 +0900

    Working On Player Join UI

---
+++ Assets/Prefabs/UI/GameUIManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameUIManager : MonoBehaviour
+{
+    [SerializeField]
+    private PlayerUIManager[] playerUIManager;
+
+    public void OnPlayerJoined(PlayerManager.PlayerArgs playerArgs)
+    {
+        //for (int i = 0; i < playerUIManager.Length; i++)
+        //{
+        //    PlayerUIManager selectedPlayerUIManager = playerUIManager[i];
+        //    if (selectedPlayerUIManager.HasPlayer  selectedPlayerUIManager.BoundPlayerID == playerArgs.PlayerId)
+        //    {
+        //        return;
+        //    }
+        //}
+
+        for (int i = 0; i < playerUIManager.Length; i++)
+        {
+            PlayerUIManager selectedPlayerUIManager = playerUIManager[i];
+            if (!selectedPlayerUIManager.HasPlayer)
+            {
+                selectedPlayerUIManager.BindPlayer(playerArgs);
+                return;
+            }
+        }
+    }
+
+    public void OnPlayerDismissed(PlayerManager.PlayerArgs playerArgs)
+    {
+        for (int i = 0; i < playerUIManager.Length; i++)
+        {
+            PlayerUIManager selectedPlayerUIManager = playerUIManager[i];
+            if (selectedPlayerUIManager.HasPlayer && selectedPlayerUIManager.BoundPlayerID == playerArgs.PlayerId)
+            {
+                selectedPlayerUIManager.UnbindPlayer();
+                return;
+            }
+        }
+    }
+}
